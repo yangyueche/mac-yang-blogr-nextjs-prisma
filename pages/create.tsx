dc82c72bbcf1630daa953d88b0dc0ec292a22e3e
yangyueche <yangyueche@gmail.com>
2022-12-31 04:55:15 -0800

    [bugFix]

--- pages/create.tsx
+++ pages/create.tsx
@@ -82,19 +82,4 @@ const Draft: React.FC = () => {
   );
 };
 
-const submitData = async (e: React.SyntheticEvent) => {
-  e.preventDefault();
-  try {
-    const body = { title, content };
-    await fetch("/api/post", {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify(body),
-    });
-    await Router.push("/drafts");
-  } catch (error) {
-    console.error(error);
-  }
-};
-
 export default Draft;
